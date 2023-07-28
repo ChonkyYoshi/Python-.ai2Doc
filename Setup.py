c721325197f967304dfc3ef1fb5c8beb0c06802b
ChonkyYoshi <agosta.enzowork@gmail.com>
2023-07-28 16:33:02 +0200

    added Setup files

---
+++ Setup.py
@@ -0,0 +1,16 @@
+from cx_Freeze import setup, Executable
+
+includefiles = []
+includes = []
+excludes = []
+
+setup(
+    name='Illustrator2Doc',
+    version='0.1',
+    description='Python GUI App to prep/clean up Illustrator files',
+    author='Enzo Agosta',
+    author_email='agosta.enzowork@gmail.com',
+    options={'build_exe': {'includes': includes, 'excludes': excludes,
+             'include_files': includefiles}},
+    executables=[Executable('Main.py', base="Win32GUI")]
+)
